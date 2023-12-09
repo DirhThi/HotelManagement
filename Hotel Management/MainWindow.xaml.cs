5b07c19c7cb9327827c8ae4a8fb7d760df8c150c
DirhThi <20520771@gm.uit.edu.vn>
2023-12-09 22:32:38 +0700

    navigation update

---
+++ Hotel Management/MainWindow.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotel_Management
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        // Start: MenuLeft PopupButton //
+        private void btnHome_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnHome;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Quản lý thuê phòng";
+            }
+        }
+
+        private void btnHome_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnDashboard_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnDashboard;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Quản lý các phòng";
+            }
+        }
+
+        private void btnDashboard_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnProducts_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnProducts;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Quản lý hóa đơn";
+            }
+        }
+
+        private void btnProducts_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnProductStock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnProductStock;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Quản lý dịch vụ";
+            }
+        }
+
+        private void btnProductStock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnOrderList_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnOrderList;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Báo cáo";
+            }
+        }
+
+        private void btnOrderList_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnBilling_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnBilling;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Quản lý nhân sự";
+            }
+        }
+
+        private void btnBilling_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnPointOfSale_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnPointOfSale;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Quản lý khách hàng";
+            }
+        }
+
+        private void btnPointOfSale_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+
+        private void btnSecurity_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (Tg_Btn.IsChecked == false)
+            {
+                Popup.PlacementTarget = btnSecurity;
+                Popup.Placement = PlacementMode.Right;
+                Popup.IsOpen = true;
+                PopupText.Text = "Đăng xuất";
+            }
+        }
+
+        private void btnSecurity_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+        /* private void btnSetting_MouseEnter(object sender, MouseEventArgs e)
+         {
+             if (Tg_Btn.IsChecked == false)
+             {
+                 Popup.PlacementTarget = btnSetting;
+                 Popup.Placement = PlacementMode.Right;
+                 Popup.IsOpen = true;
+                 Header.PopupText.Text = "Setting";
+             }
+         }*/
+
+        private void btnSetting_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Popup.Visibility = Visibility.Collapsed;
+            Popup.IsOpen = false;
+        }
+        // End: MenuLeft PopupButton //
+
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            fContainer.Navigate(new System.Uri("Pages/Quanlythuephong/Quanlythuephong.xaml", UriKind.RelativeOrAbsolute));
+            title.Text = "Quản lý thuê phòng";
+        }
+
+        private void btnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void loaded(object sender, RoutedEventArgs e)
+        {
+            //  btnHome.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        }
+    }
+}
