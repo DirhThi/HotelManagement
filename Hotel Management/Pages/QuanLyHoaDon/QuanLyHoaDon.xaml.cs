0af1a2a85c33e213e4e8e0e3e2dc98076f0aac08
Trí Đỗ Minh <20520827@ms.uit.edu.vn>
2023-12-15 20:29:21 +0700

    [Create] Quan Ly Hoa Don

--- Hotel Management/Pages/QuanLyHoaDon/QuanLyHoaDon.xaml.cs
+++ Hotel Management/Pages/QuanLyHoaDon/QuanLyHoaDon.xaml.cs
@@ -23,6 +23,36 @@ namespace Hotel_Management.Pages.QuanLyHoaDon
         public QuanLyHoaDon()
         {
             InitializeComponent();
+
+            List<bill> users = new List<bill>();
+            users.Add(new bill() { Number = 1, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013"});
+            users.Add(new bill() { Number = 2, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 3, Status = "Chưa thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "null" });
+            users.Add(new bill() { Number = 4, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 5, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 6, Status = "Chưa thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "null" });
+            users.Add(new bill() { Number = 7, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 8, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 9, Status = "Chưa thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "null" });
+            users.Add(new bill() { Number = 10, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 11, Status = "Đã thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "1/1/2013" });
+            users.Add(new bill() { Number = 12, Status = "Chưa thanh toán", Sum = 500000, CreateDate = "12/12/2012", PayDate = "null" });
+            GD_Hoa_Don.ItemsSource = users;
+
+            
+        }
+
+        public class bill
+        {
+            public int Number { get; set; }
+
+            public string Status { get; set; }
+
+            public int Sum { get; set; }
+
+            public string CreateDate { get; set; }
+
+            public string PayDate { get; set; }
         }
     }
 }
