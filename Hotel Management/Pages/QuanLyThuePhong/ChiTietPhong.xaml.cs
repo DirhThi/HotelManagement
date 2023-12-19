2be6f64f0438db35cd083b1f0e91a054cee38d13
DirhThi <20520771@gm.uit.edu.vn>
2023-12-19 17:27:47 +0700

    chitietdatphong ui update

--- Hotel Management/Pages/QuanLyThuePhong/ChiTietPhong.xaml.cs
+++ Hotel Management/Pages/QuanLyThuePhong/ChiTietPhong.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace Hotel_Management.Pages.QuanLyThuePhong
 {
@@ -20,6 +21,19 @@ namespace Hotel_Management.Pages.QuanLyThuePhong
     /// </summary>
     public partial class ChiTietPhong : Page
     {
+        List<int> numberList = new List<int>() { 101, 102, 103, 201, 203, 301, 304, 401, 402, 403 };
+        List<service> ServiceList = new List<service> {
+              new service {name="Nước suối",price="10.000",quantity=0,imagesource="/Assets/Images/aquafina.png"},
+              new service {name="Sting",price="15.000",quantity=0,imagesource="/Assets/Images/sting.jpg"},
+              new service {name="Cocacola",price="15.000",quantity=0,imagesource="/Assets/Images/cocacola.png"},
+              new service {name="Redbull",price="20.000",quantity=0,imagesource="/Assets/Images/redbull.png"},
+              new service {name="Mì ly",price="15.000",quantity=0,imagesource="/Assets/Images/mily.png"},
+              new service {name="Bún bò",price="40.000",quantity=0,imagesource="/Assets/Images/bunbo.png"},
+              new service {name="Phở bò",price="40.000",quantity=0,imagesource="/Assets/Images/phobo.png"},
+
+            };
+
+        
         public ChiTietPhong(string maphong)
         {
             InitializeComponent();
@@ -28,6 +42,8 @@ namespace Hotel_Management.Pages.QuanLyThuePhong
             DatePicker2.SelectedDate = DateTime.Now.AddDays(1);
             DatePicker2.BlackoutDates.AddDatesInPast();
             DatePicker2.BlackoutDates.Add(new CalendarDateRange(DateTime.Now));
+            serviceIC.ItemsSource = ServiceList;
+
 
         }
 
@@ -88,5 +104,36 @@ namespace Hotel_Management.Pages.QuanLyThuePhong
                 borderngay.Visibility = Visibility.Hidden;
             }
         }
+
+        private void truservicebtn_Click(object sender, RoutedEventArgs e)
+        {
+            service Service = (sender as Button).DataContext as service;
+            if (Service.quantity > 0)
+            {
+                int temp = Service.quantity;
+                temp--;
+                Service.quantity = temp;
+            }
+            serviceIC.Items.Refresh();
+
+        }
+
+
+        private void congservicebtn_Click(object sender, RoutedEventArgs e)
+         {
+            service Service = (sender as Button).DataContext as service;
+            int temp = Service.quantity;
+            temp++;
+            Service.quantity = temp;
+            serviceIC.Items.Refresh();
+
+        }
+    }
+    public  class service 
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+        public int quantity { get; set; }
+        public string imagesource { get; set; }
     }
 }
