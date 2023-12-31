7a73e6481613e3a5f341d3590c9f721db6c432d0
Trí Đỗ Minh <20520827@ms.uit.edu.vn>
2023-12-31 20:51:25 +0700

    [Thêm]: + Truy xuất danh sách nhân sự từ DB, cập nhật số lượng nhân sự khi xóa. + Xóa một hoặc nhiều nhân sự trong DB (bỏ comment để dùng).

--- Hotel Management/Pages/QuanLyNhanSu/QuanLyNhanSu.xaml.cs
+++ Hotel Management/Pages/QuanLyNhanSu/QuanLyNhanSu.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 namespace Hotel_Management.Pages.QuanLyNhanSu
 {
     /// <summary>
@@ -20,7 +23,10 @@ namespace Hotel_Management.Pages.QuanLyNhanSu
     /// </summary>
     public partial class QuanLyNhanSu : Page
     {
-        List<Khachhang> users = new List<Khachhang> {
+        static MongoClient client = new MongoClient("mongodb+srv://vitalis:arthur010203@cluster0.4opqwlz.mongodb.net/");
+        static IMongoDatabase database = client.GetDatabase("HotelManagement");
+        IMongoCollection<BsonDocument> collectionEmployee = database.GetCollection<BsonDocument>("User");
+        List<NhanVien> employeeList = new List<NhanVien>();/* {
              new Khachhang() { TenNV="Nguyễn Đình Thi",   Sodienthoai="0909090909",CCCD="12345678900966123", Ngaysinh= "23/12/2023", Email = "Mail@gmail.com", Role = "Nhân Viên"},
              new Khachhang() { TenNV="Nguyễn Đình Thi",   Sodienthoai="0909090909",CCCD="12345678900966123", Ngaysinh= "23/12/2023", Email = "Mail@gmail.com", Role = "Nhân Viên"},
              new Khachhang() { TenNV="Nguyễn Đình Thi",   Sodienthoai="0909090909",CCCD="12345678900966123", Ngaysinh= "23/12/2023", Email = "Mail@gmail.com", Role = "Nhân Viên"},
@@ -35,25 +41,50 @@ namespace Hotel_Management.Pages.QuanLyNhanSu
             new Khachhang() { TenNV="Nguyễn Đình Thi",   Sodienthoai="0909090909",CCCD="12345678900966123", Ngaysinh= "23/12/2023", Email = "Mail@gmail.com", Role = "Nhân Viên"},
 
 
-            };
+            };*/
         public QuanLyNhanSu()
         {
             InitializeComponent();
-            DGNhanVien.ItemsSource = users;
+            LayNhanVien(collectionEmployee);
+            textSoLuong.Text = "Số lượng: " + employeeList.Count.ToString();
+            DGNhanVien.ItemsSource = employeeList;
             autoorder();
         }
 
         private void autoorder()
         {
             int t = 1;
-            for (int i = 0; i < users.Count; i++)
+            for (int i = 0; i < employeeList.Count; i++)
             {
-                users[i].stt = t;
+                employeeList[i].stt = t;
                 t++;
             }
         }
 
-        public class Khachhang
+        public void LayNhanVien(IMongoCollection<BsonDocument> collectionEmployee)
+        {
+            string employeeName;
+            string employeePhone;
+            string employeeId;
+            string employeeDoB;
+            string employeeEmail;
+            string employeeRole;
+            List<BsonDocument> documentsEmployee = collectionEmployee.Find(new BsonDocument()).ToList();
+            employeeList.Clear();
+            foreach (BsonDocument employee in documentsEmployee)
+            {
+                employeeName = employee["userName"].AsString;
+                employeeId = employee["idNumber"].AsString;
+                employeePhone = employee["phoneNumber"].AsString;
+                employeeDoB = employee["dateOfBirth"].ToLocalTime().ToShortDateString();
+                employeeEmail = employee["email"].AsString;
+                employeeRole = employee["userRole"].AsString;
+                employeeList.Add(new NhanVien() { TenNV = employeeName, Sodienthoai = employeePhone, CCCD = employeeId, Ngaysinh = employeeDoB, Email = employeeEmail, Role = employeeRole });
+            }
+
+        }
+
+        public class NhanVien
         {
             public int stt { get; set; }
             public string TenNV { get; set; }
@@ -64,6 +95,74 @@ namespace Hotel_Management.Pages.QuanLyNhanSu
             public string Role { get; set; }
 
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DGNhanVien.SelectedItems.Count != 0)
+            {
+                MessageBoxButton button = MessageBoxButton.YesNo;
+                MessageBoxResult result = MessageBox.Show("Xóa khách hàng đã chọn ?", "Cảnh báo", button, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    List<NhanVien> items = DGNhanVien.SelectedItems.Cast<NhanVien>().ToList();
+
+                    //comment đến.....
+                    foreach (NhanVien item in items)
+                    {
+                        employeeList.Remove(item);
+                    }
+                    DGNhanVien.ItemsSource = employeeList;
+                    //...đây và bỏ comment phía dưới nếu muốn xóa dữ liệu trong db
+
+
+                    /* Xóa dữ liệu trong db
+                    XoaNhanVien(collectionEmployee, items);
+                    LayNhanVien(collectionEmployee);
+                    */
+
+                    textSoLuong.Text = "Số lượng: " + employeeList.Count.ToString();
+                    DGNhanVien.ItemsSource = employeeList;
+                    autoorder();
+                    DGNhanVien.Items.Refresh();
+                }
+            }
+        }
+
+        public void XoaNhanVien(IMongoCollection<BsonDocument> collectionEmployee, List<NhanVien> items)
+        {
+            foreach (NhanVien item in items)
+            {
+                collectionEmployee.DeleteOne(x => x["idNumber"] == item.CCCD);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            MessageBoxButton button = MessageBoxButton.YesNo;
+            MessageBoxResult result = MessageBox.Show("Xóa khách hàng?", "Cảnh báo", button, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                NhanVien item = ((FrameworkElement)sender).DataContext as NhanVien;
+
+                //comment đến.....
+                employeeList.Remove(item);
+                DGNhanVien.ItemsSource = employeeList;
+                //...đây và bỏ comment phía dưới nếu muốn xóa dữ liệu trong db
+
+                /* Xóa dữ liệu trong db
+                collectionEmployee.DeleteOne(x => x["idNumber"] == item.CCCD);
+                LayNhanVien(collectionEmployee);
+                */
+
+                textSoLuong.Text = "Số lượng: " + employeeList.Count.ToString();
+                DGNhanVien.ItemsSource = employeeList;
+                autoorder();
+                DGNhanVien.Items.Refresh();
+
+            }
         }
     }
 }
