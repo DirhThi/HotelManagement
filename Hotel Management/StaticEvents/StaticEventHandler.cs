f680731235c033bdc415c602eb97304d32866b9f
Nguyễn Thành Trung <20521015@gm.uit.edu.vn>
2024-01-04 18:32:37 +0700

    Fix

--- Hotel Management/StaticEvents/StaticEventHandler.cs
+++ Hotel Management/StaticEvents/StaticEventHandler.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -11,6 +12,7 @@ namespace Hotel_Management.StaticEvents
     public static class StaticEventHandler
     {     
         public static event Action OnCustomerUpdated;
+        public static readonly Regex _regex = new Regex("[^0-9.-]+");
         public static void OnCustomerUpdatedEvent()
         {
             OnCustomerUpdated?.Invoke();
